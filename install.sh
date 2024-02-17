c351007c8d71c8c34942c2debaa46ac7e5982231
Matteo Baratto <teo.baratto.97@gmail.com>
2024-02-17 15:37:23 +0100

    Added installation script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+packages=(
+	"git" 
+	"base-devel"
+	"uxterm"
+	"i3-wm"
+	"polybar"
+	"rofi"
+	"feh"
+	"network-manager-applet"
+	"flameshot"
+)
+
+packages_yay=(
+	"autotiling"
+	"lightdm-mini-greeter"
+)
+
+if pacman -Qq yay &>/dev/null; 
+then
+	echo "yay is already installed"
+else
+	mkdir ~/opt
+	git clone https://aur.archlinux.org/yay.git ~/opt/yay
+	cd ~/opt/yay
+	makepkg -si
+fi
+
+sudo pacman -S ${packages[*]}
+yay -S ${packages_yay[*]} 
